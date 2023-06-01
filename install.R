174311e4d88e7862b95030fd4b7197b644feb01c
Joel Nitta <joelnitta@gmail.com>
2023-06-01 15:00:15 +0900

    Update R packages

--- install.R
+++ install.R
@@ -1,1 +1,24 @@
-install.packages(c("tidyverse", "sf"))
+install.packages(
+  c(
+    "canaper",
+    "CoordinateCleaner",
+    "devtools",
+    "phyloregion",
+    "rgbif",
+    "rnaturalearth",
+    "sf",
+    "tidyverse")
+  )
+
+install.packages(
+  "rnaturalearthhires",
+  repos = "https://ropensci.r-universe.dev",
+  type = "source")
+
+install.packages(
+  "ftolr",
+  repos = c(
+    "https://joelnitta.r-universe.dev/",
+    "https://cran.rstudio.com/"
+  ),
+  dep = TRUE)
