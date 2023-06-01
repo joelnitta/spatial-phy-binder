9a1cdba4c795f7fe2c889cd61e57c159bbd5bcbf
Joel Nitta <joelnitta@gmail.com>
2023-06-01 17:39:58 +0900

    Install packages from renv

--- install.R
+++ install.R
@@ -1,24 +1,18 @@
-install.packages(
-  c(
-    "canaper",
-    "CoordinateCleaner",
-    "devtools",
-    "phyloregion",
-    "rgbif",
-    "rnaturalearth",
-    "sf",
-    "tidyverse")
+install.packages("renv")
+
+# Grab renv.lock corresponding to a specific commit at
+# https://github.com/joelnitta/spatial-phy-workshop
+sha <- "61bde4aa6f9bc4765170378324c225c7cb0abbea"
+
+# Download renv lock file and write out
+renv <- readLines(
+  paste0(
+    "https://raw.githubusercontent.com/joelnitta/spatial-phy-workshop/",
+    sha,
+    "/renv.lock"
+    )
   )
+writeLines(renv, "renv.lock")
 
-install.packages(
-  "rnaturalearthhires",
-  repos = "https://ropensci.r-universe.dev",
-  type = "source")
-
-install.packages(
-  "ftolr",
-  repos = c(
-    "https://joelnitta.r-universe.dev/",
-    "https://cran.rstudio.com/"
-  ),
-  dep = TRUE)
+# Install packages
+renv::restore()
